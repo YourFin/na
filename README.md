d648335da135adabd34d4ae4ec16dfc24dcaa7f2
YourFin <d.junkpobox+github@gmail.com>
2017-04-01 18:50:22 -0500

    added examples, removed some debug stuff

---
+++ README.md
@@ -0,0 +1,70 @@
+# na.sh: The ultimate in cli netctl-auto laziness!
+Do you find that typing out "netctl-auto switch-to" takes too darn long? Do you wish to be prompted to launah wifi-menu way too much? Do you like having wrapper scripts for your wrapper scripts? Then na.sh is for YOU!
+
+## Features:
+- Easy profile switching with case-insensitve partial name matching
+- Searches SSID's along with profile names
+- Quick handlinng of extraneous matches
+- Shortcuts for common related commands with no hard-to-reach dashes
+- More greps than your `history`
+
+## Usage:
+
+- `na`
+List all profiles, show which are active with an `*` and give number prompts for easy switching
+- `na [NAME]`
+Switch to profile `NAME`. Should NAME not exist, looks for partial matches on all profile names and SSIDs on all readable profiles. In the case of one match, switch to it. Multiple, display a numbered list and prompt for answer.
+- `na r`
+Restart netctl-auto systemd service.
+- `na p [DOMAIN]`
+Ping [DOMAIN] until a response is recieved and display latenay. Useful for notifying you when you are reconnected to the internet after restarting netctl or easily testing your connection. Defaults to `google.com`.
+
+## Examples:
+
+$ na
+[1]:   wlp58s0-- DEN Airport Free WiFi
+[2]:   wlp58s0-BNGuest
+[3]: * neptune
+[4]:   cherry
+[5]:   wlp58s0-PokeStop
+[6]:   gs2
+[7]:   phone
+[8]:   newPhone
+[9]:   4148
+[10]:   twotreehill
+[11]:   grinnellS
+[12]:   Silverstar209
+[13]:   mihs
+[14]:   Lilytail
+[15]:   gs
+[16]:   room5
+[17]:   leahAkins
+[18]:   seatac
+[19]:   wlp58s0-Seasprite
+[w] launch wifi-menu
+[e] exit
+e
+
+$ na w
+Possible matches:
+[1]: newPhone
+[2]: seatac
+[3]: twotreehill
+[4]: wlp58s0-BNGuest
+[5]: wlp58s0-- DEN Airport Free WiFi
+[6]: wlp58s0-PokeStop
+[7]: wlp58s0-Seasprite
+[w] launch wifi-menu
+[e] exit
+e
+
+$ na p 
+Connected: Latency with google.com ~19.8 ms
+
+## Installation:
+Clone the repo and symlink na.sh to somewhere in your  `$PATH`, or add 
+```
+alias na='/path/to/this/cloned/repository/na.sh'
+```
+to your `.bashrc`.
+
---
+++ na.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+# Init
+netctlAuto=`systemctl | grep netctl-auto | sed 's/.service.*/.service/g'`
+wifimenucommand="wifi-menu"
+wifimenucommandsuffix=""
+chmodNewWifiMenu='true'
+netctlProfilePath='/etc/netctl'
+
+#make newlines the split character for arrays
+IFS=$'\n'
+
+if [[ ! -z $netctlAuto ]] ; then
+    switchcommand="netctl-auto switch-to"
+    stopallcommand="netctl-auto disable-all"
+    wifimenucommandsuffix="systemctl restart $netctlAuto"
+    restartcommand="systemctl restart $netctlAuto"
+else 
+    switchcommand="netctl switch-to"
+    stopallcommand="netctl stop-all"
+    restartcommand="systemctl restart $(systemctl | grep netctl | sed 's/.service.*/.service/g')"
+fi
+
+# add sudo if necessary
+if [[ $EUID -ne 0 ]] ; then
+    wifimenucommand="sudo $wifimenucommand"
+    restartcommand="sudo $restartcommand"
+
+    if [[ ! -z $wifimenucommandsuffix ]] ; then
+        wifimenucommandsuffix="sudo $wifimenucommandsuffix"
+    fi
+
+    if [[ -z $netctlAuto ]] ; then 
+        switchcommand="sudo $switchcommand"
+        stopallcommand="sudo $stopallcommand"
+    fi
+fi
+
+
+#finish splicing wifimenu together
+if [ -z "$wifimenucommandsuffix" ]; then wifimenucommand="$wifimenucommand && $wifimenucommandsuffix"; fi
+wifimenucommand="echo Lanuching wifi-menu... ; $wifimenucommand"
+
+switchProfile () 
+{
+	if ! [ -e "/etc/netctl/$1" ] ; then
+		echo "Error: profile: \"$1\" does not exist"
+		exit 1
+	elif ! [ -r "/etc/netctl/$1" ] ; then
+		switchcommand="sudo $switchcommand"
+	fi
+	echo "Switching to $1..."
+	eval $switchcommand $1
+}
+switchProfilePrompt () 
+{
+	echo "Switch to profile $1? (Y/n)"
+	read userinput
+	if [ -z $(echo "$userinput" | grep -E '[Nn][Oo]?') ]; then
+		switchProfile $1
+	fi
+}
+
+listAndChoose () 
+{
+    connections=( $(netctl-auto list) )
+    current='0'
+    for (( ii=0; ii<${#connections[@]}; ii++ ));
+    do
+        echo "[$(($ii + 1))]: ${connections[ii]}"
+    done
+    if `hash wifi-menu 2>/dev/null` ; then #supress hash error
+        echo '[w] launch wifi-menu'
+    fi
+    echo '[e] exit'
+    
+    read userinput
+    if [ -z $(echo $userinput | sed 's/[0-9]*//') ] && [ ! -z "$userinput" ] && [ "$userinput" -le "${#connections[@]}" ]
+        # if userinput is entirely numbers and a valid index and non-null
+    then
+	switchProfile $(echo ${connections[(($userinput - 1))]} | cut -c 3-)
+
+    elif $(hash wifi-menu 2>/dev/null) && [[ "$userinput" == "w" || "$userinput" == "W" ]]; then
+        eval $wifimenucommand
+    fi #by default do nothing
+}
+
+switchHandler ()
+{
+	if [ -e "$netctlProfilePath/$1" ] && [ ! -d "$netctlProfilePath/$1" ] ; then
+		switchProfile $1
+	else 
+		inputProfiles=( $(ls -p $netctlProfilePath | grep -v '/' | #List only non-directories
+			grep -o '.*'$1'.*') $(grep -i -s -l "ESSID=.*$1.*" $netctlProfilePath/* | sed "s:$netctlProfilePath/::") )
+		if [ ! -z $inputProfiles ] ; then
+			#remove duplicates
+			eval inputProfiles=( $(printf "%q\n" "${inputProfiles[@]}" | sort -u) )
+			matchingProfiles=()
+			for thing in ${inputProfiles[@]} ; do
+				if [ ! -d "$thing" ] ; then
+					matchingProfiles+=("$thing")
+				fi
+			done
+
+			#ask to switch to the first if it's the only one
+			if [ "${#matchingProfiles[@]}" -eq "1" ] ; then
+				switchProfilePrompt "${matchingProfiles[0]}"
+			else
+				echo "Possible matches:"
+				for (( ii=0; ii<${#matchingProfiles[@]}; ii++ ));
+				do
+					echo "[$(($ii + 1))]: ${matchingProfiles[ii]}"
+				done
+        			echo '[w] launch wifi-menu'
+				echo '[e] exit'
+			        read userinput
+			        if [ -z $(echo $userinput | sed 's/[0-9]*//') ] && [ ! -z "$userinput" ] && [ "$userinput" -le "${#matchingProfiles[@]}" ]
+			            # if userinput is entirely numbers and a valid index and non-null
+			        then
+					switchProfile "${matchingProfiles[(($userinput - 1))]}"
+
+			        elif $(hash wifi-menu 2>/dev/null) && [[ "$userinput" == "w" || "$userinput" == "W" ]]; then
+			            eval $wifimenucommand
+			        fi #by default do nothing
+			fi
+		else
+			echo "No matching profiles found; launch wifi-menu? (Y/n)"
+			read userinput
+			if [ -z $(echo "$userinput" | grep -E '[Nn][Oo]?') ]; then
+				eval $wifimenucommand
+			fi
+		fi
+	fi
+}
+
+if [[ -z $1 ]]; then
+    listAndChoose
+elif [[ "$1" == "restart" || "$1" == "r" ]]; then
+    eval $restartcommand
+elif [[ "$1" == "ping" || "$1" == "p" ]]; then
+    if [[ -z $2 ]]; then
+        pingLocation='google.com'
+    else
+        pingLocation=$2
+    fi
+    while [[ ! `ping -c 1 -W 1 $pingLocation 2>/dev/null` ]]; do sleep 0.1; done
+    echo Connected: $(ping -c 1 -w 0.5 $pingLocation 2>/dev/null | grep -o 'time=[0-9/.]* ms' | sed "s/time/Latency with $pingLocation/" | sed 's/=/ ~/')
+else #insert something here that tries to correct wifi names or takes parts; save for later
+	switchHandler $1
+fi
